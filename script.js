51d6949eef37e8d03aa7ecc528d7651c8f86243e
Shahid Chaudhary <133282511+chshahid119@users.noreply.github.com>
2023-11-09 11:15:26 +0500

    smooth scrolling added using js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const mobile_nav_icon = document.getElementById("mobile-nav-btn");
 const header = document.querySelector(".header");
 
 mobile_nav_icon.addEventListener("click", function () {
-  console.log("clicked");
+  // console.log("clicked");
   header.classList.toggle("nav-open");
 });
 
@@ -11,6 +11,37 @@ const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.innerText = currentYear;
 
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+// console.log(allLinks);
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    // Scroling back to top when click on logo of site
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    // Scroll to other links when click on any btn or nav menu
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      // console.log(sectionEl);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    // Close mobile navigation after click on nav link
+    if (link.classList.contains("main-nav-link")) {
+      header.classList.toggle("nav-open");
+    }
+  });
+});
 // Fixing flexbox gap property missing in some Safari Version
 function checkFlexGap() {
   var flex = document.createElement("div");
