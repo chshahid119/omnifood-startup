25db812febaf806b024b0cdfc8082e228747e8f8
Shahid Chaudhary <133282511+chshahid119@users.noreply.github.com>
2023-11-09 08:01:31 +0500

    add js for safari browser support

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ mobile_nav_icon.addEventListener("click", function () {
   console.log("clicked");
   header.classList.toggle("nav-open");
 });
+
+// Fixing flexbox gap property missing in some Safari Version
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexboox-gap");
+}
+
+checkFlexGap();
