0cf016ca60bbe78da72d80f90c2f7922ac3dc343
Shahid Chaudhary <133282511+chshahid119@users.noreply.github.com>
2023-11-17 00:24:02 +0500

    console log removed from js file

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const mobile_nav_icon = document.getElementById("mobile-nav-btn");
 const header = document.querySelector(".header");
 
 mobile_nav_icon.addEventListener("click", function () {
-  // console.log("clicked");
   header.classList.toggle("nav-open");
 });
 
@@ -13,12 +12,11 @@ yearEl.innerText = currentYear;
 
 // Smooth scrolling animation
 const allLinks = document.querySelectorAll("a:link");
-// console.log(allLinks);
+
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
-    console.log(href);
 
     // Scroling back to top when click on logo of site
     if (href === "#") {
@@ -30,7 +28,7 @@ allLinks.forEach(function (link) {
     // Scroll to other links when click on any btn or nav menu
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
-      // console.log(sectionEl);
+
       sectionEl.scrollIntoView({
         behavior: "smooth",
       });
@@ -49,7 +47,6 @@ const sectionHeroEl = document.querySelector(".section-hero");
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    console.log(ent);
 
     if (ent.isIntersecting === false) {
       document.body.classList.add("sticky");
@@ -77,7 +74,6 @@ function checkFlexGap() {
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight === 1;
   flex.parentNode.removeChild(flex);
-  console.log(isSupported);
 
   if (!isSupported) document.body.classList.add("no-flexboox-gap");
 }
